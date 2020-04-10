9480298f928c5e8d0800b03e736221f92d001710
Daniel <daniel.mueller08@googlemail.com>
2020-04-11 01:19:33 +0200

    Added new pages and toggle buttons for zeitschaltuhren

--- rbp_webserver.py
+++ rbp_webserver.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect
 import os
 app = Flask(__name__)
 
@@ -15,9 +15,17 @@ def funky():
 def turn_socket_on_off(remote, socket, turn):
     try: 
         os.system('rfsniffer play {}.{}{}'.format(remote, socket, turn))
-        return render_template('funky.html')
+        return redirect(url_for('funky'))
     except:
         return render_template('ooops.html')
 
+@app.route('/settings')
+def settings():
+	return render_template('settings.html')
+
+@app.route('/music')
+def music():
+	return render_template('music.html')
+
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
