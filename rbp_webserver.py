fbf0d3041162f4afa3c2ec5ce8e51aa5602554b2
Daniel <daniel.mueller08@googlemail.com>
2020-04-13 14:02:05 +0200

    replaced tabs with spaces fixed behavior of wrong displayed active tab in sidebar made buttons in /funky asynchronous added shortcut icons for phones

--- rbp_webserver.py
+++ rbp_webserver.py
@@ -12,96 +12,99 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 class Data(object):
-	url = '/home/pi/rbp_webserver/rbp_webserver/data.json'
+    url = '/home/pi/rbp_webserver/rbp_webserver/data.json'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/funky')
+@app.route('/funky', methods=['GET', 'POST'])
 def funky():
-	with open(Data.url) as file:
-		data = json.load(file)
-		categories = dict()
-		#create dictionary where the keys are the different categories and the value is a list of the dicts containing a socket of this category
-		for funk in data["funksteckdosen"]: 
-			category = categories.get(funk["category"], None)
-			if category is None:
-				categories[funk["category"]] = [funk]
-			else:
-				category.append(funk)
-				categories[funk["category"]] = category
-		#convert dictionary to a list where each entry is a dictionary with one key/val pair being the name of this category 
-		#and the other key/val pair being the list of dictionaries for this category (for template)
-		cat_list = list()
-		for key, val in categories.items():
-			temp_dict = dict()
-			temp_dict["name"] = key
-			temp_dict["sockets"] = val
-			cat_list.append(temp_dict)
+    if request.method == 'GET':
+        with open(Data.url) as file:
+            data = json.load(file)
+            categories = dict()
+            #create dictionary where the keys are the different categories and the value is a list of the dicts containing a socket of this category
+            for funk in data["funksteckdosen"]: 
+                category = categories.get(funk["category"], None)
+                if category is None:
+                    categories[funk["category"]] = [funk]
+                else:
+                    category.append(funk)
+                    categories[funk["category"]] = category
+            #convert dictionary to a list where each entry is a dictionary with one key/val pair being the name of this category 
+            #and the other key/val pair being the list of dictionaries for this category (for template)
+            cat_list = list()
+            for key, val in categories.items():
+                temp_dict = dict()
+                temp_dict["name"] = key
+                temp_dict["sockets"] = val
+                cat_list.append(temp_dict)
 
-	return render_template('funky.html', categories=cat_list)
+        return render_template('funky.html', categories=cat_list)
 
-@app.route('/funky/<string:remote>_<int:socket>_<string:turn>')
-def turn_socket_on_off(remote, socket, turn):
-    try: 
-        os.system('rfsniffer play {}.{}{}'.format(remote.lower(), socket, turn))
-        return redirect(url_for('funky'))
-    except:
-        return redirect(url_for('ooops'))
+    elif request.method == 'POST':
+        btn_id = request.form['btn_id']
+        btn_id = btn_id.split('+')
+        try:
+            os.system('rfsniffer play {}.{}{}'.format(btn_id[0].lower(), btn_id[1], btn_id[2]))
+            return 'OK'
+        except:
+            return redirect(url_for('ooops'))
+        
 
 @app.route('/settings', methods=['GET', 'POST'])
 def settings():
-	if request.method == 'GET':
-		with open(Data.url) as file:
-			data = json.load(file)
-			forms = list()
-			for funk in data["funksteckdosen"]:
-				form = SocketAssignmentForm(MultiDict(funk))
-				forms.append(form)
-		return render_template('settings.html', forms=forms)
-	elif request.method == 'POST':
-		form_data = request.form['form_data']
-		form_data = form_data.replace('=y&', '=true&')
-		form_data = form_data.split('&formbreak&')
-		form_data = form_data[:-1]
-		json_list = list()
-		for form in form_data:
-			form_dict = urllib.parse.parse_qs(form)
-			form_dict = {key:val[0] for key,val in form_dict.items()}
-			form_obj = SocketAssignmentForm(MultiDict(form_dict))
-			form_obj_data = form_obj.data
-			form_obj_data.pop('csrf_token', None)
-			json_list.append(form_obj_data)
-		with open(Data.url, 'r') as file:
-			data = json.load(file)
-		data["funksteckdosen"] = json_list
-		with open(Data.url, 'w') as file:
-			json.dump(data, file, indent=4)
+    if request.method == 'GET':
+        with open(Data.url) as file:
+            data = json.load(file)
+            forms = list()
+            for funk in data["funksteckdosen"]:
+                form = SocketAssignmentForm(MultiDict(funk))
+                forms.append(form)
+        return render_template('settings.html', forms=forms)
+    elif request.method == 'POST':
+        form_data = request.form['form_data']
+        form_data = form_data.replace('=y&', '=true&')
+        form_data = form_data.split('&formbreak&')
+        form_data = form_data[:-1]
+        json_list = list()
+        for form in form_data:
+            form_dict = urllib.parse.parse_qs(form)
+            form_dict = {key:val[0] for key,val in form_dict.items()}
+            form_obj = SocketAssignmentForm(MultiDict(form_dict))
+            form_obj_data = form_obj.data
+            form_obj_data.pop('csrf_token', None)
+            json_list.append(form_obj_data)
+        with open(Data.url, 'r') as file:
+            data = json.load(file)
+        data["funksteckdosen"] = json_list
+        with open(Data.url, 'w') as file:
+            json.dump(data, file, indent=4)
 
-		return redirect(url_for('index'))
+        return redirect(url_for('index'))
 
-	
+    
 
 @app.route('/music', methods=['GET', 'POST'])
 def music():
-	if request.method == 'GET':
-		volume=20
-		form = AudioOutputForm(audio_output='box')
-		return render_template('music.html', volume=volume, form=form)
-	elif request.method == 'POST':
-		volume = request.form.get('volume', None)
-		if volume is not None:
-			print(volume)
-		audio_output = request.form.get('audio_output', None)
-		if audio_output is not None:
-			print(audio_output)
+    if request.method == 'GET':
+        volume=20
+        form = AudioOutputForm(audio_output='box')
+        return render_template('music.html', volume=volume, form=form)
+    elif request.method == 'POST':
+        volume = request.form.get('volume', None)
+        if volume is not None:
+            print(volume)
+        audio_output = request.form.get('audio_output', None)
+        if audio_output is not None:
+            print(audio_output)
 
-		return 'OK'
+        return 'OK'
 
 @app.route('/ooops')
 def ooops():
-	return render_template('ooops.html')
+    return render_template('ooops.html')
 
 
 if __name__=='__main__':
